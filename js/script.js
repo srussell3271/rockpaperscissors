f7fb4f191182b2daeae02035c4be01e1d0ffa5e7
Shannon Russell <s.russell3271@gmail.com>
2018-11-20 23:31:43 +0000

    new inputs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 // *************************** YOUR CODE BELOW *******************************
-$("#shoot").click(function(){
-    $("#userChoice").text($("#input").val());
-    });
 
+function computerminds(){
+    
+    var randomChoice = Math.random()
+
+    if(randomChoice < 0.333333){
+        $("#computerChoice").text("rock");
+    }
+    else if(randomChoice < 0.666666){
+        $("#computerChoice").text("paper");
+    }
+    else{
+        $("#computerChoice").text("scissors");
+    }
+console.log(randomChoice)
+
+}
+
+$("#shoot").click(function(){
+
+var input =$("#input").val();
+
+    $("#userChoice").text(input);
+    $("#computerChoice").text(computerminds());
+
+});
+var input =$("#input").val();
+
+if(input === computerminds()){
+    alert("TIE");
+    console.log("tie");
+}
 
 //******************TEST EARLY AND OFTEN USING console.log() ******************
+
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
