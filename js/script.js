dd5501cc6f3ab5185a93f9e251318ff90c8e4141
Shannon Russell <s.russell3271@gmail.com>
2018-11-26 22:44:11 +0000

    trying to fix result

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-// *************************** YOUR CODE BELOW *******************************
+// variables
 
+// coding
+
+//GLOBAL VARIABLES
+/* global $ */
 function computerminds(){
     
-    var randomChoice = Math.random()
+    var randomChoice = Math.random();
 
-    if(randomChoice < 0.333333){
-        $("#computerChoice").text("rock");
+    if(randomChoice < 0.333333){ 
+       $("#computerChoice").text("rock");
     }
     else if(randomChoice < 0.666666){
         $("#computerChoice").text("paper");
@@ -13,33 +17,69 @@ function computerminds(){
     else{
         $("#computerChoice").text("scissors");
     }
-console.log(randomChoice)
-
+        console.log(randomChoice);
 }
 
+function results(){
+    var inputs = $("#input").val();
+    // var paper= $("#computerChoice").text("paper");
+    // var rock= $("#computerChoice").text("rock");
+    // var scissors= $("#computerChoice").text("scissors");
+    var computerChoice =$("#computerChoice").text();
+
+    if(computerChoice==="paper" && inputs==="paper"){
+        $("#result").text("It's A Tie");
+        console.log("Tie");
+
+        }
+    else if(computerChoice==="paper" && inputs==="scissors"){
+        $("#result").text("Tie");
+                console.log("player Wins");
+
+        }
+    else if(computerChoice==="paper"  && inputs==="rock"){
+        $("#result").text("Player Wins");
+                console.log("Player Wins");
+}
+    else if(computerChoice==="rock" && inputs==="scissors"){
+        $("#result").text("Computer Wins");
+            console.log("computer Wins");
+
+        }
+    else if(computerChoice==="rock" && inputs==="paper"){
+        $("#result").text("Player Wins");
+        console.log("player Wins");
+        }
+    else if(computerChoice==="rock" && inputs==="rock"){
+        $("#result").text("It's A Tie");
+            console.log("Tie");
+
+        }
+    else if(computerChoice==="scissors" && inputs==="scissors"){
+        $("#result").text("It's A Tie");
+        console.log("Tie");
+        }    
+    else if(computerChoice==="scissors" && inputs==="paper"){
+        $("#result").text("Computer Wins");
+            console.log("computer Wins");
+
+        }
+    else if(computerChoice==="scissors" && inputs==="rock"){
+        $("#result").text("Player Wins");
+                console.log("player Wins");
+
+        } 
+    else{
+        alert("Please type rock, paper, or scissors as your answer");
+        }
+    }
+
+// working buttons
 $("#shoot").click(function(){
+    var input = $("#input").val();
 
-var input =$("#input").val();
-
+    $("#result").text(results());
     $("#userChoice").text(input);
     $("#computerChoice").text(computerminds());
 
 });
-var input =$("#input").val();
-
-if(input === computerminds()){
-    alert("TIE");
-    console.log("tie");
-}
-
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
